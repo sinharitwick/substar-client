f0b3316e3ba42a9c7885bdbe0df052616e89f7d5
sinharitwick <sritwick10@gmail.com>
2025-12-30 00:44:31 +0530

    listed subscriptions

---
+++ src/components/Subscription.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Chip, Typography } from '@mui/material'
+
+function Subscription({ sub }: any) {
+  return (
+    <Accordion key={sub.subscriptionId} sx={{ backgroundColor: '#000000', color: '#ffffff', mb: 1 }}>
+        <AccordionSummary>
+          <Typography component="span">{sub.serviceName}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Box>
+                <Chip label={sub.status} variant="outlined" sx={(sub.status === 'ACTIVE' ? { color: 'success.main', borderColor: 'success.main' } : { color: 'error.main', borderColor: 'error.main' } )} />
+            </Box>
+            <Box>
+                <Typography component="span" variant='h6'>₹{sub.cost}/</Typography>
+                <Typography component="span" variant='h6'>{sub.billingCycle.charAt(0).toLowerCase()}</Typography>
+            </Box>
+            Renewal: {sub.renewalDate.slice(0, 10)}
+        </AccordionDetails>
+    </Accordion>
+  )
+}
+
+export default Subscription
--- src/pages/SubscriptionsPage.tsx
+++ src/pages/SubscriptionsPage.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Typography } from '@mui/material';
+import { useParams } from 'react-router';
+import Subscription from '../components/Subscription';
 
 function SubscriptionsPage() {
+    let params = useParams();
+    const [subscription, setSubscription] = useState([]);
+    useEffect(() => {
+        const getSubscriptions = async() => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_SUBSTAR_API_BASE_URL}/subscriptions`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('auth-token')}`,
+                    },
+                    params: {
+                        category: params.category,
+                    }
+                });
+                setSubscription(response.data);
+            } catch (error) {
+                console.log('Failed to fetch subscriptions', error);
+            }
+        }
+        getSubscriptions();
+    }, [])
   return (
-    <div>SubscriptionsPage</div>
+    <div>
+        <Typography fontWeight='bold' variant='h6' sx={{ fontFamily: 'monospace', mb: 4 }}> {params.category} Subscriptions</Typography>
+        { subscription.map((sub: any) => (
+            <Subscription sub={sub} />
+        ))}
+    </div>
   )
 }
 
