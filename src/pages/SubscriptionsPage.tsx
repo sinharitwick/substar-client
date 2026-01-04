3aa9cb9c77a28e3c55f727c49423489b69269622
sinharitwick <sritwick10@gmail.com>
2026-01-05 00:58:49 +0530

    monthly expense summary added for each category

--- src/pages/SubscriptionsPage.tsx
+++ src/pages/SubscriptionsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import axios from 'axios';
 import { Fab, Typography } from '@mui/material';
 import { useParams } from 'react-router';
@@ -57,10 +57,20 @@ function SubscriptionsPage() {
             toast.error("Failed to delete subscription");
         }
     }
+    const monthlyExpenses = useMemo(() => {
+        return subscriptions.reduce((total: number, sub: any) => {
+            if(sub.status === 'ACTIVE') {
+                if(sub.billingCycle === 'MONTHLY') return total + sub.cost;
+                return total + Math.round(sub.cost / 12);
+            }
+            return total;
+        }, 0);
+    }, [subscriptions]);
   return (
     <>
         <Navbar />
-        <Typography fontWeight='bold' variant='h6' sx={{ fontFamily: 'monospace', mb: 4 }}>Your {params.category} Subscriptions</Typography>
+        <Typography fontWeight='bold' sx={{ fontFamily: 'monospace', mb: 2 }}>You're spending ₹{monthlyExpenses}/month on {params.category} subscriptions </Typography>
+        <Typography fontWeight='bold' variant='h6' sx={{ fontFamily: 'monospace', mb: 2 }}>Your {params.category} Subscriptions</Typography>
         { subscriptions.map((sub: any) => (
             <Subscription key={sub.subscriptionId} sub={sub} onOpenEditDialog={() => handleEditOpen(sub)} onDelete={() => handleDeleteSubscription(sub.subscriptionId)} />
         ))}
